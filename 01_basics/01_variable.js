af95a3c9e963e70f08cb1bae4b2eacc50263d219
Nikhil Saini <78295068+nikhilsaini04@users.noreply.github.com>
2024-02-23 10:27:27 +0000

    how to decleare variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,5 +1,14 @@
 const accountID=1234
 let accountEmail="nikhilsainiimt@gmail.com"
 var accountPassword="3456"
+/*
+Prefer not to use var
+because of issue of block scope and functional scop
+*/
+let accountState;
 accountCity="Greater Noida"
-console.log(accountEmail)
+
+//accountID=21  Not allowed
+accountCity="Bulandshahr"  
+console.log(accountID) 
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])   
