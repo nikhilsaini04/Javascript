f8b75060c950c3746d20055c3a64aa92622058f9
Nikhil Saini <78295068+nikhilsaini04@users.noreply.github.com>
2024-02-23 11:14:39 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+ "use strict";// treat all js code as newer version
+ // alert(3+3)// we are  using nodejs, not browser
+
+ let name="Nikhil Saini"
+ let age=23
+ let isLoggedIn=false
+ 
+
+ //number --> 2 to power 53
+ // bigint  
+ // string --> ""
+ // boolean --> true/false
+ // null --> standalone value
+ // undefined ->
+ // symbol --> unique 
+ // object
+ console.log(typeof "Nikhil")
+ console.log(typeof age)
+ console.log(typeof null) // object
+ console.log(typeof undefined)// undefined
